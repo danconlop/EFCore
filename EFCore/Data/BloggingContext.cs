9d15d6820749d9a176c0baa4e8533026e7c61fa8
Daniel <danconlop@gmail.com>
2022-04-21 10:31:05 -0700

    Add project files.

---
+++ EFCore/Data/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Data
+{
+    public class BloggingContext : DbContext
+    {
+        // para indicarle a EF que las entidades se conviertan en tablas
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Blogging;Integrate Security=True");
+
+        }
+    }
+}
---
+++ EFCore/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; } // Llave foranea
+        public Blog Blog { get; set; } // Para poder acceder a los datos de la otra entidad
+
+    }
+}
